42be640d90a77e7d23dbe2857d1f13d456691f89
Adil Saleem <165240677+adilsaleemmansoor@users.noreply.github.com>
2024-03-28 14:19:20 +0500

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	char op;
+	double num1,num2;
+	
+	cout << "Enter the Operator ( +, -, *, /, % ) :"<<endl;
+	cin >> op;
+	
+	cout << "Enter First Number :"<<endl;
+	cin >> num1;
+	
+	cout << "Enter Second Number :"<<endl;
+	cin >> num2;
+	
+	switch ( op )
+	{
+		case '+':
+			cout << num1 <<" + " << num2 << " = "<<(num1+num2)<<endl;
+			break;
+			
+		case '-':
+			cout << num1 <<" - " << num2 << " = "<<(num1-num2)<<endl;
+			break;
+			
+		case '*':
+			cout << num1 <<" * " << num2 << " = "<<(num1*num2)<<endl;
+			break;
+			
+		case '/':
+			if(num2 !=0.0)
+				cout << num1 <<" / " << num2 << " = "<<(num1/num2)<<endl;
+			else 
+				cout<<"Divided By Zero Situation";
+			break;	
+			
+		default :
+			cout << op << "is invalid Operator"<<endl;
+	}
+	
+	
+}
